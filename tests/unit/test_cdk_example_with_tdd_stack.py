fceb1dafdb99ea4d86712e5769413d12df8b6e3e
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-03-25 00:51:53 -0600

    feat: add a unit test to validate the resource and policy

--- tests/unit/test_cdk_example_with_tdd_stack.py
+++ tests/unit/test_cdk_example_with_tdd_stack.py
@@ -1,15 +1,16 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-
+from pytest import fixture, Mark
 from cdk_example_with_tdd.cdk_example_with_tdd_stack import CdkExampleWithTddStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cdk_example_with_tdd/cdk_example_with_tdd_stack.py
-def test_sqs_queue_created():
-    app = core.App()
-    stack = CdkExampleWithTddStack(app, "cdk-example-with-tdd")
-    template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+@fixture(scope='session')
+def template():
+    test_stack = core.Stack()
+    stack = CdkExampleWithTddStack(test_stack, 'cdk-example-with-tdd')
+    return assertions.Template.from_stack(stack)
+
+
+def test_dynamodb_table(template):
+    template.resource_count_is('AWS::DynamoDB::Table', 1)
+    template.has_resource('AWS::DynamoDB::Table', { 'DeletionPolicy': 'Delete' })
