fb78738f40030df1b98b663a75664fec2edf7019
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-03-25 00:53:11 -0600

    feat: Add a DynamoDB table with a partition key

--- cdk_example_with_tdd/cdk_example_with_tdd_stack.py
+++ cdk_example_with_tdd/cdk_example_with_tdd_stack.py
@@ -2,6 +2,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_dynamodb as DynamoDB,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -10,10 +12,15 @@ class CdkExampleWithTddStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        table_name = 'Wheater'
+        table = DynamoDB.Table(
+            self, 
+            table_name, 
+            partition_key=DynamoDB.Attribute(
+                name='ZipCode', 
+                type=DynamoDB.AttributeType.STRING,
+            ), 
+            removal_policy=RemovalPolicy.DESTROY
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkExampleWithTddQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+      
